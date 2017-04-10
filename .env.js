95e14f440c84a1c2232c40c93db95d7a207238fb
chuayewwee@gmail.com <chuayewwee@gmail.com>
2017-04-10 14:41:32 +0800

    first commit

---
+++ .env.js
@@ -0,0 +1,7 @@
+# Bot Framework Variables
+MICROSOFT_APP_ID=
+MICROSOFT_APP_PASSWORD=
+
+
+# Subscription URL: https://www.microsoft.com/cognitive-services/en-us/subscriptions
+MICROSOFT_VISION_API_KEY=c84d0e1aaaff482b9014a570927bdbf2
